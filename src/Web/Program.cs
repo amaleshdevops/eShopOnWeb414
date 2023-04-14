659a68e43d8304710fb1f0a5f9f966eba678eef0
amaleshdevops <130656051+amaleshdevops@users.noreply.github.com>
2023-04-13 23:54:01 -0700

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -20,6 +20,7 @@ using Azure.Identity;
 using Microsoft.eShopWeb.Web.Pages;
 using Microsoft.FeatureManagement;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
